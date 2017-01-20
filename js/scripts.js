22beffd42636416cedd6f69c78327ed5e2d6a183
xa <xiaesmeamen@me.com>
2017-01-20 10:15:52 -0800

    debugg backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,25 @@
 // Back-end Logic//
 
-var pingPongGame = function(number) {
-  var game = [];
-  for (i = 1; i <= number; i ++){
-  if ( i % 15 === 0 && i !== 0) {
-    game.push("ping-pong");
-  } else if (i % 3 === 0 && i % 15 !== 0) {
-    game.push("ping");
-  } else (i % 5 && i !== 0){
-  game.push("pong");
-    }
-  };
-return game;
-};
+ var game = function(number){
+   var pingPongGame = [];
+   for (i = 1; i <= number; i ++){
+     if (i % 3 === 0 && i % 5 !== 0){
+       pingPongGame.push("ping!");
+     } else if (i % 5 === 0 && i & 3 !== 0){
+       pingPongGame.push("pong!");
+     } else if (i % 15 === 0 && i !== 0){
+       pingPongGame.push("ping-pong!");
+     }
+     else {
+       pingPongGame.push(i);
+     }
+   };
+    return pingPongGame;
+ };
 
 
 // Front-end Logic //
-$(document).ready(function(event){
-  event.preventDefault();
-  alert("backend is ready");
-});
+// $(document).ready(function(event) {
+//   event.preventDefault();
+//   alert("backend is ready");
+// });
